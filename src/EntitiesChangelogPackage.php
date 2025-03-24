563be753c5b6ddd72e7075e31086b7d2e8a7c33e
Patrick Morpey <patrick@morphp.nl>
2025-03-24 12:33:56 +0100

    Replaced placeholders

--- src/EntitiesChangelogPackage.php
+++ src/EntitiesChangelogPackage.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Medas\Placeholder;
+namespace Medas\EntitiesChangelog;
 
 use Medas\Core\AsSingleton;
 use Medas\ServiceManager\BasePackage;
 
-class PlaceholderPackage extends BasePackage
+class EntitiesChangelogPackage extends BasePackage
 {
     use AsSingleton;
 
